2fdcff38ec0a216a35ffd6c726f9e1b58426bf1c
DarkhanDaurenov <takishevdarkhan@gmail.com>
2024-02-19 18:03:00 +0600

    add src/video.py

--- src/video.py
+++ src/video.py
@@ -11,11 +11,24 @@ class Video:
 
     def __init__(self, video_id: str) -> None:
         self._video_id = video_id
-        video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',id=video_id).execute()
-        self.title = str(video_response['items'][0]['snippet']['title'])
-        self.count = str(video_response['items'][0]['statistics']['viewCount'])
-        self.like_count = int(video_response['items'][0]['statistics']['likeCount'])
-        self.comment_count = int(video_response['items'][0]['statistics']['commentCount'])
+        try:
+            video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',id=video_id).execute()
+            if video_response.get('items'):
+                self.title = str(video_response['items'][0]['snippet']['title'])
+                self.count = str(video_response['items'][0]['statistics']['viewCount'])
+                self.like_count = int(video_response['items'][0]['statistics']['likeCount'])
+                self.comment_count = int(video_response['items'][0]['statistics']['commentCount'])
+            else:
+                self.title = None
+                self.count = None
+                self.like_count = None
+                self.comment_count = None
+        except Exception as e:
+            print(f"Произошла ошибка при получении данных для видео с идентификатором. {video_id}: {e}")
+            self.title = None
+            self.count = None
+            self.like_count = None
+            self.comment_count = None
 
     def __str__(self):
         return f"{self.title}"
